4a2d55f924d03a0a1f486a2a2abb0b759916c34f
Mueez374 <mueezahmadaslam5@gmail.com>
2026-01-14 21:55:06 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ total = sum(numbers)
 mean = sum(numbers)/5
 print(total)
 print(mean) 
+# new code underneath 
+
+score= 0
+attempt = 2
+correctanswer= "green"
+while attempt > 0 :
+      guess = input("what is the colour of grass")
+      if guess== correctanswer :
+          score = score + 1
+          break 
+      else:
+            attempt = attempt - 1
+            if attempt > 0:
+              print("try again")
+            else: 
+               print("Game over")
+print("Your Score is", score)
+  
+                
+           
+
